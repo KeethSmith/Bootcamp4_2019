da2b5e303daacc6740312b8821155f5d0416defc
Keeth Smith <32115132+Spelunker30@users.noreply.github.com>
2019-10-30 17:24:27 -0400

    commit

--- src/components/BuildingList.js
+++ src/components/BuildingList.js
@@ -1,14 +1,30 @@
 import React from 'react';
 
 class BuilingList extends React.Component {
+	selectedUpdate() {
+		//Here you will need to update the value of the filter with the value from the textbox
+		const id = this.myValue.id
+		this.props.selectedUpdate(id)
+	}
+
 	render() {
 		//console.log('This is my directory file', this.props.data);
-		const { data } = this.props;
+		const { data, filterText, selectedUpdate} = this.props;		  
 
-		const buildingList = data.map(directory => {
+		const buildingList = data
+		.filter(name => {
+			//remove names that do not match current filterText
+			return name.name.indexOf(filterText) >= 0
+		  })
+		
+		.map(directory => 
+			{
 			return (
-				<tr key={directory.id}>
-					<td>{directory.code} </td>
+				<tr 
+				key={directory.id} 
+				onClick={() => selectedUpdate(directory.id)}
+				>
+					<td> {directory.code} </td>
 					<td> {directory.name} </td>
 				</tr>
 			);
--- src/components/ViewBuilding.js
+++ src/components/ViewBuilding.js
@@ -1,13 +1,39 @@
 import React from 'react';
 
+
 class ViewBuilding extends React.Component {
 	render() {
+		const {selectedBuilding,data} = this.props;
+		const buildingName = this.props.data.selectedBuilding
+		const viewBuilding = selectedBuilding
+		const buildingList = data
+		.filter(id => {
+			//remove names that do not match current filterText
+			return id.name.indexOf(buildingName) 		  })
+		.map(directory => 
+			{
+			return (
+				<tr 
+				key={directory.id}>
+					<td> {directory.code} </td>
+					<td> {directory.name} </td>
+					<td> {directory.address} </td>
+					
+				</tr>
+			);
+		});
+
 		return (
 			<div>
 				<p>
 					{' '}
 					<i>Click on a name to view more information</i>
 				</p>
+				{buildingName}
+				<p></p>
+				{viewBuilding}
+				<p></p>
+				{buildingList}
 			</div>
 		);
 	}
